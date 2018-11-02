7b5c7c4e039e1b139ef0253c36ebb58da0311565
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2018-11-02 00:52:34 -0400

    add service worker to the app

---
+++ serviceWorker.js
@@ -0,0 +1,72 @@
+const cacheName = 'restaurant_reviews_1';
+
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/public/css/styles.min.css',
+  '/public/css/responsive.min.css',
+  '/public/js/dbhelper.min.js',
+  '/public/js/main.min.js',
+  '/public/js/restaurant_info.min.js',
+];
+
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    }),
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        }),
+      ),
+    ),
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+
+  const request = event.request.url.includes('/restaurant.html')
+    ? new Request('/restaurant.html')
+    : event.request;
+
+  event.respondWith(
+    caches.match(request).then(response => response || fetch(request)),
+  );
+});
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
